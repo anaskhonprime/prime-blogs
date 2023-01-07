90dc4e58f46ba3e9e239e22a7402be08a5438e4d
anaskhonprime <smlife202@gmail.com>
2023-01-07 17:38:15 +0900

    Initial Commit for Prime Blogs

---
+++ src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <Navbar></Navbar>
+</template>
+
+<script>
+
+import Navbar from './Navbar';
+
+export default {
+
+    data() {
+        return {
+
+        }
+    },
+
+    components : {
+        Navbar,
+    },
+
+    methods: {
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from './router/router.js';
+import './style/tailwind.css';
+
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDIdYii98QJzN2I9_LWaBn42WGwbuz3km8",
+    authDomain: "vueblog-13514.firebaseapp.com",
+    projectId: "vueblog-13514",
+    storageBucket: "vueblog-13514.appspot.com",
+    messagingSenderId: "5840313268",
+    appId: "1:5840313268:web:d33d3720ad95afcc5cb0e1",
+    measurementId: "G-BH06B8ZTFV"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
+
+
+createApp(App).use(router).mount('#app')
