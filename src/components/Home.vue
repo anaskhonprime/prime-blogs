b864364a355ffe403f72bb37ed1c27e5e770846c
anaskhonprime <smlife202@gmail.com>
2023-01-11 15:53:26 +0900

    Updated Login Pages

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,6 @@
 <template>
     <Navbar></Navbar>
     <Sidebar/>
-   
      
 </template>
 
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,30 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from '../components/Home'
 
+
+import Login from '../account/Login'
+import Register from '../account/Register'
+import Dashboard from '../account/Dashboard'
+
+
 const routes = [{
-    path: '/',
-    component: Home,
-}, ];
+        path: '/',
+        component: Home,
+    },
+
+    {
+        path: '/login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        component: Register,
+    }, {
+        path: '/Dashboard',
+        component: Dashboard,
+    },
+
+];
 
 
 
