6ece985882d0f4c39b3243c3bb6850c51143c61e
anaskhonprime <smlife202@gmail.com>
2023-01-15 22:23:00 +0900

    Updated Admin Panel, Firestore Upload  Components

--- src/account/General.vue
+++ src/account/General.vue
@@ -150,7 +150,7 @@
             <span class="sr-only">Open dropdown</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path></svg>
         </button>
-        <!-- Dropdown menu -->
+       
         <div id="dropdown" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700">
             <ul class="py-1" aria-labelledby="dropdownButton">
             <li>
